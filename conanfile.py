f40a0a6f9dd25e5ead0cd8c43f6c4dfc97047ff1
Jerry W <jerrywiltse@gmail.com>
2017-12-31 18:18:38 -0500

    Update url field in conanfile [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ from conans import ConanFile, tools
 class BoostSpiritConan(ConanFile):
     name = "boost_spirit"
     version = "1.66.0"
-    url = "https://github.com/bincrafters/conan-boost-spirit"
+    url = "https://github.com/bincrafters/conan-boost_spirit"
 
     lib_short_names = ["spirit"]
     is_in_cycle_group = True
