41297d62d09474de78727c394fef26afc3abe541
Jerry W <jerrywiltse@gmail.com>
2018-08-20 21:57:53 -0400

    use generic test_package conanfile

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
 from conans import ConanFile, CMake, tools, RunEnvironment
 import os
@@ -9,22 +10,10 @@ class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
 
-    def requirements(self):
-        boost_deps = ['algorithm', 'array', 'assert', 'concept_check', 'config', 'core', 'endian', 'filesystem', 'foreach', 'function', 'function_types', 'fusion', 'integer', 'io', 'iostreams', 'iterator', 'lexical_cast', 'locale', 'math', 'move', 'mpl', 'optional', 'phoenix', 'predef', 'preprocessor', 'proto', 'range', 'regex', 'smart_ptr', 'spirit', 'static_assert', 'throw_exception', 'tti', 'type_traits', 'typeof', 'unordered', 'utility', 'variant']
-        for lib in boost_deps:
-            self.requires("boost_" + lib + "/1.67.0@" + self.user + "/" + self.channel)
-        if False:
-            if not tools.os_info.is_windows:
-                self.requires("openmpi/3.0.0@bincrafters/stable")
-
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
     def test(self):
-        with tools.environment_append(RunEnvironment(self).vars):
-            if self.settings.os == "Windows":
-                self.run(os.path.join("bin", "test_package"))
-            else:
-                self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ['DYLD_LIBRARY_PATH'], os.path.join("bin", "test_package")))
+        self.run(os.path.join("bin", "test_package"), run_environment=True)
