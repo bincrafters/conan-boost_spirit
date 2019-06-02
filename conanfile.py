8dc90de27be8fdcf979ef5aa6001ba83e5a52513
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:08 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,13 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostSpiritConan(base.BoostBaseConan):
     name = "boost_spirit"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-spirit"
-    lib_short_names = ["spirit"]
-    header_only_libs = ["spirit"]
-    cycle_group = "boost_cycle_group_c"
-    b2_requires = ["boost_cycle_group_c"]
+    version = "1.70.0"
